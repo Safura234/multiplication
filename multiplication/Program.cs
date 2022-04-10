3ef5650429db327a3e9f458101cfe401d5c40356
Safura234 <safuramg@code.edu.az>
2022-04-10 12:43:56 +0400

    https://github.com/Safura234/multiplication.git

---
+++ multiplication/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace multiplication
+{
+    internal class Program
+    {
+        private static bool result;
+
+        static int Main(string[] args)
+        {
+            Console.WriteLine("multiplication table");
+            int num1 = 0;
+            int num2 = 0;
+            Console.WriteLine("Enter the num1");
+            Console.WriteLine("Enter the num2");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            int multiplicationTable = num1 * num2;
+        
+            {
+                int result = num1 * num2;
+                return result;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
